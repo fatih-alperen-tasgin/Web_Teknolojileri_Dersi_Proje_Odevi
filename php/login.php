77218458241d9a3deaae20f873fbdeeb8388dd02
Fatih Alperen Taşğın <fatihaslanim@gmail.com>
2025-05-25 13:23:05 +0300

    Update login.php

---
+++ php/login.php
@@ -0,0 +1,22 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $kullanici = $_POST["kullanici"];
+  $sifre = $_POST["sifre"];
+
+  // Örnek öğrenci bilgileri
+  $dogruKullanici = "b2412100001@sakarya.edu.tr";
+  $dogruSifre = "b2412100001";
+
+  if ($kullanici == $dogruKullanici && $sifre == $dogruSifre) {
+    echo "<h2 style='text-align:center; margin-top: 50px;'>Hoş geldiniz <strong>$sifre</strong>!</h2>";
+  } else {
+    // Başarısız giriş → login.html'e yönlendirme
+    header("Location: login.html");
+    exit();
+  }
+} else {
+  // Form POST ile gelmemişse login sayfasına dön
+  header("Location: login.html");
+  exit();
+}
+?>
