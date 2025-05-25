eb258e3bf31bbb32a8baa799504c13947935665f
Fatih Alperen Taşğın <fatihaslanim@gmail.com>
2025-05-25 20:24:38 +0300

    login kısmındaki js dosyasında ufak bir düzeltme yapıldı.

--- js/login.js
+++ js/login.js
@@ -1,14 +1,17 @@
 function validateForm() {
-  const email = document.getElementById("kullanici").value;
-  const sifre = document.getElementById("sifre").value;
+  const email = document.getElementById("kullanici").value.trim();
+  const sifre = document.getElementById("sifre").value.trim();
+
   if (!email || !sifre) {
     alert("Lütfen tüm alanları doldurunuz.");
     return false;
   }
-  const emailPattern = /^[a-z0-9]+@ogr.sakarya\.edu\.tr$/i;
+
+  const emailPattern = /^[a-z0-9._%+-]+@ogr\.sakarya\.edu\.tr$/i;
   if (!emailPattern.test(email)) {
     alert("Lütfen geçerli bir Sakarya Üniversitesi e-posta adresi giriniz.");
     return false;
   }
+
   return true;
 }
