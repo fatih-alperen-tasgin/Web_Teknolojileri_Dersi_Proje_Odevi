22ee1c910761357dd08cedb316359b746191faf0
Fatih Alperen Taşğın <fatihaslanim@gmail.com>
2025-06-03 21:11:48 +0300

    HTML ve PHP dosyalarında düzenlemeler yapıldı: buton rengi değiştirildi, kullanıcı adı hatalı mesajı düzeltildi.

--- php/login.php
+++ php/login.php
@@ -8,15 +8,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $dogruSifre = "b241210079";
 
   if ($kullanici == $dogruKullanici && $sifre == $dogruSifre) {
-    echo "<h2 style='text-align:center; margin-top: 50px;'>Hoş geldiniz <strong>$sifre</strong>!</h2>";
+    echo "<h2 style='text-align:center; margin-top: 50px;'>Hoş geldiniz <strong>$kullanici</strong>!</h2>";
   } else {
-    // Başarısız giriş → login.html'e yönlendirme
-    header("Location: login.html");
+    // Başarısız giriş → login.html'e yönlendirme, hata mesajı ile
+    header("Location: http://localhost/Web_Teknolojileri_Dersi_Proje_Odevi/login.html?error=Kullanıcı adı veya şifre yanlış");
     exit();
   }
 } else {
-  // Form POST ile gelmemişse login sayfasına dön
-  header("Location: login.html");
+  // Form POST ile gelmemişse login sayfasına dön, hata mesajı ile
+  header("Location: http://localhost/Web_Teknolojileri_Dersi_Proje_Odevi/login.html?error=Geçersiz istek, lütfen tekrar giriş yapın");
   exit();
 }
 ?>
